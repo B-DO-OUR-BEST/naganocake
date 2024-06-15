c25921e1d9e7f866d0eee098d700439fe3082cef
EC2 Default User <ec2-user@ip-172-31-10-197.ap-northeast-1.compute.internal>
2024-06-15 07:26:42 +0000

    xxx

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,44 @@
 Rails.application.routes.draw do
   
   scope module: :public do
-    devise_for :users, controllers: {
-      registrations: 'registrations',
-      sessions: 'sessions',
-    resource :homes, only: [:top,:about]
+    
+    resource :homes, only: [:top,:about] 
     resources :items, only: [:index,:show]
-    resource :customers, only: [:show,:edit,:update]
+    resource :customers, only: [:show,:edit,:update] do
       member do
         get :unsubscribe
         get :withdraw
       end
-    resources :cart_items, only: [:index,:update,:create,:destroy]
+    end
+    resources :cart_items, only: [:index,:update,:create,:destroy] do
       member do
         get :destroy_all
       end
-    resources :orders, only: [:new,:create,:index,:show]
+    end
+    resources :orders, only: [:new,:create,:index,:show] do
       member do
         get :confirm
         get :thanks
-      end
-    resources :addresses, only: [:index, edit, create, update, destroy]  
+      end 
+    end
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]  
   end
 
   namespace :admin do
-    devise_for :admins, controllers: {
-      sessions: 'sessions',
-    resource :homes,only: [:top]
+    
+    resource :homes, only: [:top]
     resources :items, only: [:index,:new,:create,:show,:edit,:update]
-    resources :genres,only: [:index,:create,:edit,:update]
-    resources :customers,only: [:index,:show,:edit,:update]
-    resources :orders,only: [:show,:update]
-    resources :order_details,only: [:update]
+    resources :genres, only: [:index,:create,:edit,:update]
+    resources :customers, only: [:index,:show,:edit,:update]
+    resources :orders, only: [:show,:update]
+    resources :order_details, only: [:update]
   end
-
+  devise_for :customers, controllers: {
+      registrations: 'public/registrations',
+      sessions: 'public/sessions',
+    }
+  devise_for :admins, controllers: {
+      sessions: 'admin/sessions',
+    }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
