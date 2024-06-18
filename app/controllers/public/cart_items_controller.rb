3aaeee3ebd5fc86b79b40bd4781a1366c2499401
YuIshikawa <166091720+29yu03@users.noreply.github.com>
2024-06-18 08:09:42 +0000

    pullするのでcommit

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_cart.cart_items
+    @cart_item = CartItem.all
   end
 
   def update
@@ -13,4 +15,10 @@ class Public::CartItemsController < ApplicationController
 
   def create
   end
+
+  private
+    def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
+  end
+
 end
