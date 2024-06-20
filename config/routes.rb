c3bcb6aa4c84e860fbf295c2d8f49be9b84dd0e6
EC2 Default User <ec2-user@ip-172-31-10-197.ap-northeast-1.compute.internal>
2024-06-20 10:15:51 +0000

    管理者情報seed記述

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   namespace :admin do
 
     # resource :homes, only: [:top]
-    root 'homes#top'
+    get '/' => 'homes#top'
     resources :items, only: [:index,:new,:create,:show,:edit,:update]
     resources :genres, only: [:index,:create,:edit,:update]
     resources :customers, only: [:index,:show,:edit,:update]
