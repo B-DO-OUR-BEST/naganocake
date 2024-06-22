63429af168af16f27d33b79f52829ddba35bfc4b
yk-sora <165569035+yk-sora@users.noreply.github.com>
2024-06-22 08:20:34 +0000

    エラ－解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_customer!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-
-  
   private
 
   def after_sign_in_path_for(resource)
@@ -21,16 +19,13 @@ class ApplicationController < ActionController::Base
     my_page_customers_path
   end
   
-  
-
   def after_sign_out_path_for(resource)
     root_path
   end
 
-
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :telephone_number] )
   end
 end
