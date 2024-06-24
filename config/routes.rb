9a3955cff1c510bdb45543733b04d2be3a963883
YuIshikawa <166091720+29yu03@users.noreply.github.com>
2024-06-24 12:07:38 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Rails.application.routes.draw do
     end
     resources :orders, only: [:new,:create,:index,:show] do
       collection do
-        get :confirm
+        post :confirm
         get :thanks
       end
     end
