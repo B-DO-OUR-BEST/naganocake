dd19539f18fefc614a986099547d9f17c92b28ae
yk-sora <165569035+yk-sora@users.noreply.github.com>
2024-06-24 04:57:45 +0000

    完成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,12 +9,24 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to @customer, notice: '更新が完了しました。'
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
   end
 
   def withdraw
+  end
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :password, :password_confirmation)
   end
   
 end
