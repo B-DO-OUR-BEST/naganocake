36f64a283813021e7f08416cac340148d267abcb
YuIshikawa <166091720+29yu03@users.noreply.github.com>
2024-06-24 11:12:49 +0000

    a

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,5 @@
 class Public::CustomersController < ApplicationController
-  
+
   def show
     @customer = current_customer
   end
@@ -22,11 +22,12 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
   end
-  
+
   private
 
   def customer_params
-    params.require(:customer).permit(:name, :email, :password, :password_confirmation)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana,
+    :post_code, :address, :telephone_number, :email, :password, :password_confirmation)
   end
-  
+
 end
