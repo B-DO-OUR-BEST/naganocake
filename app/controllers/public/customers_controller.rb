b3ca9826189d66bd25be7d888e086ee7d9d349a0
yk-sora <165569035+yk-sora@users.noreply.github.com>
2024-06-25 08:17:33 +0000

    あ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,5 @@
 class Public::CustomersController < ApplicationController
+
   before_action :authenticate_customer!
   
   def show
@@ -19,21 +20,15 @@ class Public::CustomersController < ApplicationController
   end
 
   def unsubscribe
-    @customer = current_customer
   end
 
   def withdraw
-    @customer = Customer.find(current_customer.id)
-    @customer.update(is_active: false)
-    reset_session
-    flash[:notice] = "Thank you for the good rating. We hope to see you again."
-    redirect_to root_path
   end
-  
+
   private
 
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :telephone_number, :email)
   end
-end
 
+end
