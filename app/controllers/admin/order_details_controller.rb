796cfcf907d036ea591d1d7a419335003507dab9
EC2 Default User <ec2-user@ip-172-31-10-197.ap-northeast-1.compute.internal>
2024-06-25 09:01:12 +0000

    kannsei

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,14 @@
 class Admin::OrderDetailsController < ApplicationController
+before_action :authenticate_admin!
+
   def update
     order_detail = OrderDetail.find(params[:id])
     order_detail.update(order_detail_params)
+
+    if all_order_details_completed?(order_detail.order)
+      update_order_status(order_detail.order, 'preparing_shipment')
+    end
+
     redirect_to admin_order_path(order_detail.order_id)
   end
 
@@ -9,5 +16,13 @@ class Admin::OrderDetailsController < ApplicationController
 
   def order_detail_params
     params.require(:order_detail).permit(:making_status)
+  end
+
+  def update_order_status(order, status)
+    order.update(status: status)
+  end
+
+  def all_order_details_completed?(order)
+    order.order_details.all? { |detail| detail.making_status == 'completed' }
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,15 +1,21 @@
 class Admin::OrdersController < ApplicationController
-	before_action :authenticate_admin!
+	 before_action :authenticate_admin!
 
-	def show
-		@order = Order.find(params[:id])
-		@order_details = @order.order_details
-	end
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+  end
 
-	def update
-    order = Order.find(params[:id])
-    order.update(order_params)
-    redirect_to admin_order_path(order)
+  def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.update(order_params)
+
+    if @order.payment_confirmed?
+      @order_details.update_all(making_status: :pending)
+    end
+
+    redirect_to admin_order_path(@order)  
   end
 
   private
